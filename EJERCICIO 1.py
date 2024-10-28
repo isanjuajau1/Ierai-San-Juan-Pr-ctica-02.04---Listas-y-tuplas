768eb13c99489d052fa7f6f1c1c72f9e5a038c63
isanjuajau1 <isanjuajau1@educacion.navarra.es>
2024-10-28 11:27:50 +0100

    EJERCICIO 1

---
+++ EJERCICIO 1.py
@@ -0,0 +1,7 @@
+#Escribir un programa que almacene las asignaturas de un curso (por ejemplo 
+#Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre por 
+#pantalla.
+
+asig = ['RETE', 'DAPI', 'SHID', 'GPIT', 'SIRC', 'SIPA']
+
+print(asig)

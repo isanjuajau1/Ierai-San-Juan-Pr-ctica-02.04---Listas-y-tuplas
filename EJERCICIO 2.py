e72441aa89b3b2313159c7f7e2f5d3cac4c92c3a
isanjuajau1 <isanjuajau1@educacion.navarra.es>
2024-10-28 11:40:39 +0100

    EJERCICIO 2

--- EJERCICIO 2.py
+++ EJERCICIO 2.py
@@ -3,3 +3,7 @@ Matemáticas, Física, Química, Historia y Lengua) en una lista y la muestre po
 pantalla el mensaje: Yo estudio <asignatura>, donde <asignatura> es cada una 
 de las asignaturas de la lista.'''
 
+asig = ['RETE', 'DAPI', 'SHID', 'GPIT', 'SIRC', 'SIPA']
+
+for i in range(len(asig)):
+    print('Yo estudio {}.'.format(asig[i]))

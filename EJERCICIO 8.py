87e017f9f9642665981eab0764ab694235d926ed
isanjuajau1 <isanjuajau1@educacion.navarra.es>
2024-11-04 10:15:59 +0100

    EJERCICIO 8

---
+++ EJERCICIO 8.py
@@ -0,0 +1,17 @@
+#Escribir un programa que pida al usuario una palabra y muestre por pantalla si 
+#es un palíndromo.
+
+p = input('Escribe una palabra: ')
+q = p
+plista = list(p) # plista = lista normal
+plist = list(q)
+plistr = plist.reverse() # plist = lista inversa
+palindromo = False
+
+if plista == plist:
+    palindromo = True
+
+if palindromo == True:
+    print('La palabra {} es palíndroma. '.format(p))
+else:
+    print('La palabra {} no es palíndroma. '.format(p))
